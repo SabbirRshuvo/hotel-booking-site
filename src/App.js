401e997c279ea8a718533cf31364d8e0ef3767c6
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-03-24 10:35:07 +0600

    last commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MiddleNav from "./Components/MiddleNav/MiddleNav";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <MiddleNav />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/MiddleNav/MiddleNav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {} from "./MiddleNav.css";
+function MiddleNav() {
+    return (
+        <div className="nav">
+            <ul id="links">
+                <li>
+                    <a href="#">Home</a>{" "}
+                </li>
+                <li>
+                    <a href="#">Roomes</a>{" "}
+                </li>
+                <li>
+                    <a href="#">Contact</a>{" "}
+                </li>
+                <li>
+                    <a href="#">About</a>{" "}
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default MiddleNav;
